ceb87f4a62fd10893e53c3255b26b6edff9cfddd
35682058 <59949448+35682058@users.noreply.github.com>
2020-01-16 14:55:00 -0600

    Redo
    Redo everything

---
+++ hello.c
@@ -0,0 +1,14 @@
+/* Hack01
+Name: Chiang Kuan Ruei
+Date: 2020/01/16
+*/
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char **argv) {
+    printf("Chiang Kuan Ruei\n");
+    return 0;
+}
+
+
+
+
